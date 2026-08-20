ceb6f7b75c648c60ecfb4d360d1ef55abd6231ba
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2026-08-20 09:01:08 +0100

    Update provider versions and bump module versions

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = "~> 1.3"
+  required_version = ">= 1.3, < 2.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.54.0"
+      version = ">= 5.0, < 7.0"
     }
     vault = {
       source  = "hashicorp/vault"
-      version = "~> 3.18.0"
+      version = ">= 5.0, < 6.0"
     }
   }
 }
@@ -21,29 +21,33 @@ terraform {
 }
 
 module "ecs-cluster" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-cluster?ref=1.0.231"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-cluster?ref=1.0.411"
 
-  stack_name                 = local.stack_name
-  name_prefix                = local.name_prefix
-  environment                = var.environment
-  aws_profile                = var.aws_profile
-  vpc_id                     = data.aws_vpc.vpc.id
-  subnet_ids                 = local.application_subnet_ids
-  ec2_key_pair_name          = var.ec2_key_pair_name
-  ec2_instance_type          = var.ec2_instance_type
-  ec2_image_id               = var.ec2_image_id
+  aws_profile = var.aws_profile
+  environment = var.environment
+  name_prefix = local.name_prefix
+  stack_name  = local.stack_name
+  subnet_ids  = local.application_subnet_ids
+  vpc_id      = data.aws_vpc.vpc.id
+
+  ec2_key_pair_name = var.ec2_key_pair_name
+  ec2_instance_type = var.ec2_instance_type
+  ec2_image_id      = var.ec2_image_id
+
   asg_max_instance_count     = var.asg_max_instance_count
   asg_min_instance_count     = var.asg_min_instance_count
-  enable_container_insights  = var.enable_container_insights
   asg_desired_instance_count = var.asg_desired_instance_count
-  scaledown_schedule          = var.asg_scaledown_schedule
-  scaleup_schedule            = var.asg_scaleup_schedule
-  enable_asg_autoscaling      = var.enable_asg_autoscaling
+
+  scaledown_schedule     = var.asg_scaledown_schedule
+  scaleup_schedule       = var.asg_scaleup_schedule
+  enable_asg_autoscaling = var.enable_asg_autoscaling
+
+  enable_container_insights   = var.enable_container_insights
   notify_topic_slack_endpoint = local.notify_topic_slack_endpoint
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.231"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.411"
 
   name_prefix = local.name_prefix
   environment = var.environment
